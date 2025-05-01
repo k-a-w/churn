7f6a47f36cff89bb5c0497fe09cd24282ff4b4ac
k-a-w <144478303+k-a-w@users.noreply.github.com>
2025-04-30 23:06:52 -0500

    fix typo in Data Ingestion section

--- churn_Spring2025/constants/__init__.py
+++ churn_Spring2025/constants/__init__.py
@@ -33,7 +33,7 @@ DATA_INGESTION_COLLECTION_NAME:str = "telco_data" #same as above
 DATA_INGESTION_DIR_NAME:str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
 DATA_INGESTION_INGESTED_DIR:str = "ingested"
-DATA_INGESTION__TRAIN_TEST_SPLIT_RATIO: float = 0.2
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
 
 """
---
+++ churn_Spring2025/entity/artifact_entity.py
@@ -0,0 +1,58 @@
+#Notes from class:
+"""
+What do the terms entity and artifact mean?
+    Entity generally refers to some sort of core concept or object (e.g.
+    data set or model); any significant conponents where managing and 
+    tracking is beneficial.
+
+    An artifact is typically a byproduct or result of the workflow (e.g.
+    trained models, processed data sets, or evaluation reports).
+
+The configuration refers to the settings and parameters which control
+    how the code and model runs (e.g. hyperparameters for training, 
+    filepaths, environment variables).
+"""
+
+from dataclasses import dataclass #allows use of decorators
+
+@dataclass
+class DataIngestionArtifact:
+    trained_file_path: str
+    test_file_path: str
+
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    message: str
+    drift_report_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score: float
+    precision_score: float
+    recall_score: float
+
+@dataclass
+class ModelTrainerArtifact:
+    is_model_accepted: bool
+    changed_accuracy: float
+    s3_model_path: str
+    trained_model_path: str
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted: bool
+    changed_accuracy: float
+    s3_model_path: str
+    trained_model_path: str
+
+@dataclass
+class ModelPusherArtifact:
+    bucket_name: str
+    s3_model_path: str
