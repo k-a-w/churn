41e8492d4998101b1889630c3cbea9c443af8639
k-a-w <144478303+k-a-w@users.noreply.github.com>
2025-05-17 17:12:33 -0500

    changed app host

--- churn_Spring2025/constants/__init__.py
+++ churn_Spring2025/constants/__init__.py
@@ -70,5 +70,6 @@ AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
 REGION_NAME = "us-east-1"
 
-APP_HOST = "127.0.0.1" #updated 5/9
+APP_HOST = "0.0.0.0" #updated to 127.0.0.1 on 5/9
+#changed again based off Fall 2024 project
 APP_PORT = 8080
